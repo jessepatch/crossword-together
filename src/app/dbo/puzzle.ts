f89a1a8883b4a49fb3bc2b7fcb37001def8f374d
Jesse <jessepatch9@gmail.com>
2020-10-21 23:35:03 -0500

    Added dbo for puzzle, line, and clues

---
+++ src/app/dbo/puzzle.ts
@@ -0,0 +1,20 @@
+export class Puzzle {
+    lines: Array<Line>;
+    acrossClues: Array<Clues>;
+    downClues: Array<Clues>;
+    size: number;
+    
+}
+
+export class Line {
+    words: Array<string>;
+
+}
+
+export class Clues {
+    direction: string;
+    clues: Array<string>;
+}
+
+
+// line: {"stow", "-", "-", "abba", "-", "ajar"};

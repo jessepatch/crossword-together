0b007842ec2450aadf92d3eed6566dac77d7a014
Jesse <jessepatch9@gmail.com>
2020-10-26 23:42:41 -0500

    Create sample crossword puzzle

--- src/app/solo-crossword/solo-crossword.component.ts
+++ src/app/solo-crossword/solo-crossword.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Puzzle } from '../dbo/puzzle';
+import { Line, Puzzle } from '../dbo/puzzle';
 
 @Component({
   selector: 'app-solo-crossword',
@@ -15,6 +15,25 @@ export class SoloCrosswordComponent implements OnInit {
    }
 
   ngOnInit() {
+    let puzzle = new Puzzle();
+    let lines = new Array<Line>();
+    let line1 = new Line();
+    let line2 = new Line();
+    let line3 = new Line();
+    let line4 = new Line();
+    let line5 = new Line();
+    let words1 = ['-', '-', 'moo'];
+    let words2 = ['-', '-', 'amp'];
+    let words3 = ['aegis'];
+    let words4 = ['flit', '-'];
+    let words5 = ['tic', '-', '-'];
+    line1.words = words1;
+    line2.words = words2;
+    line3.words = words3;
+    line4.words = words4;
+    line5.words = words5;
+    lines = [line1, line2, line3, line4, line5];
+    puzzle.lines = lines;
   }
 
   checkWord() {
@@ -22,7 +41,7 @@ export class SoloCrosswordComponent implements OnInit {
   }
 
   solveWord() {
-    
+
   }
 
   checkPuzzle() {
@@ -31,5 +50,9 @@ export class SoloCrosswordComponent implements OnInit {
 
   solvePuzzle() {
 
+  }
+
+  setPuzzle(puzzle: Puzzle) {
+    this.puzzle = puzzle;
   }
 }
